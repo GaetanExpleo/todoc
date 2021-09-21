804ef5c618f8fdf590cf67ef08f2552bea4e3876
Gaëtan Renault <gaetan.renault@expleogroup.com>
2021-09-21 17:16:51 +0200

    Commit 2:
    Room implementation

---
+++ app/src/main/java/com/cleanup/todoc/database/dao/ProjectDao.java
@@ -0,0 +1,16 @@
+package com.cleanup.todoc.database.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.cleanup.todoc.model.Project;
+
+import java.util.List;
+
+@Dao
+public interface ProjectDao {
+
+    @Query("SELECT * FROM Task WHERE id = :projectId")
+    LiveData<List<Project>> getTasks(long projectId);
+}
---
+++ app/src/main/java/com/cleanup/todoc/database/dao/todocDatabase.java
@@ -0,0 +1,35 @@
+package com.cleanup.todoc.database.dao;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+
+@Database(entities = {Project.class, Task.class}, version = 1, exportSchema = false)
+public abstract class todocDatabase extends RoomDatabase {
+
+    // --- SINGLETON ---
+    private static volatile todocDatabase INSTANCE;
+
+    // --- DAO ---
+    public abstract ProjectDao mProjectDao();
+    public abstract TaskDao mTaskDao();
+
+    // --- INSTANCE ---
+    public static todocDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (todocDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            todocDatabase.class, "todocDatabase.db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
